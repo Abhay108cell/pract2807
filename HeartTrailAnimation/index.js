9f6c262e34ba2dcbacf6ff5a0eeaff7ffc0d1b52
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-06 18:22:13 +0530

    "Added styles for span elements and updated event listener to set left and top positions"

--- HeartTrailAnimation/index.js
+++ HeartTrailAnimation/index.js
@@ -3,5 +3,8 @@ bodyE1.addEventListener("mousemove", (event)=>{
   const xPos = event.offsetX;
   const yPos = event.offsetY;
   const span1 = document.createElement("span")
+  span1.style.left = xPos + "px"
+  span1.style.top = yPos + "px"
   bodyE1.appendChild(span1)
-}) 
+})     
+
