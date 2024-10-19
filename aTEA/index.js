514c91b47d77268133e4cb9b95d7a012294a69e5
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-19 16:17:59 +0530

    "Updated index.js to fix article usage and add loop to carrers array"

--- aTEA/index.js
+++ aTEA/index.js
@@ -10,13 +10,16 @@ const carrers = ["youtuber","Entreprenur",
   updateText();
   function updateText() {
     characterIndex++
-
     containerE1.innerHTML = `
-    <h1>I am a ${carrers[carrersIndex].slice(0,characterIndex)} </h1>
+    <h1>I am ${carrers[carrersIndex].slice(0,1) === "I" ? 
+       "an" : "a"} ${carrers[carrersIndex].slice(0,characterIndex)} </h1>
     `;
     if (characterIndex === carrers[carrersIndex].length) {
       carrersIndex++;
       characterIndex = 0;
+      }
+      if (carrersIndex === carrers.length){
+        carrersIndex = 0;
       }
     setTimeout(updateText, 300);
   }
