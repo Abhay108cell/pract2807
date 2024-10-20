e1e51e7b4ee62972c081ab73eb113ddfd68830be
abhaykumar2871999@gmail.com <abhaykumar2871999@gmail.com>
2024-10-20 14:40:01 +0530

    Added script tag to index.html to link to index.js file and init it by selected element from HTML to js.

---
+++ minCalender/index.js
@@ -0,0 +1,4 @@
+const monthNameE1 = document.getElementById("month-name")
+const dayNameE1 = document.getElementById("day-name")
+const dayNum1 = document.getElementById("day-number")
+const yearE1 = document.getElementById("year")
